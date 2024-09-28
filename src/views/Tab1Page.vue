92c7cba9d5447d4ec1125f3e84de64af53ee2e62
James Plimmer <sc21jpwp@leeds.ac.uk>
2024-09-28 17:58:20 +0100

    Explored basic ionic vue features

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,22 +2,32 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Track</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <ion-fab vertical="center" horizontal="center">
+        <ion-button @click="router.push('/session')">
+          <h1>New Session</h1>
+        </ion-button>
+      </ion-fab>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton } from '@ionic/vue';
+import {  } from 'ionicons/icons';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 </script>
+
+<style scoped>
+</style>  
